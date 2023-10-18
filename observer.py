17dd64570381f5a4da0efb85f96999482ab85647
Steven Hicken <sahicken@gmail.com>
2023-10-17 23:21:25 -0700

    finished impl of city, agency

--- observer.py
+++ observer.py
@@ -3,125 +3,59 @@ from abc import ABC, abstractmethod
 from random import randrange
 from typing import List
 
-
 class Subject(ABC):
-    """
-    The Subject interface declares a set of methods for managing subscribers.
-    """
-
     @abstractmethod
     def attach(self, observer: Observer) -> None:
-        """
-        Attach an observer to the subject.
-        """
         pass
-
     @abstractmethod
     def detach(self, observer: Observer) -> None:
-        """
-        Detach an observer from the subject.
-        """
         pass
-
     @abstractmethod
     def notify(self) -> None:
-        """
-        Notify all observers about an event.
-        """
         pass
-
 
 class Agency(Subject):
-    """
-    The Subject owns some important state and notifies observers when the state
-    changes.
-    """
-
-    _state: int = None
-    """
-    For the sake of simplicity, the Subject's state, essential to all
-    subscribers, is stored in this variable.
-    """
-
+    _state: str = ""
+    _name: str = ""
     _observers: List[Observer] = []
-    """
-    List of subscribers. In real life, the list of subscribers can be stored
-    more comprehensively (categorized by event type, etc.).
-    """
-
+    def __init__(self, name) -> None:
+        self._name=name
     def attach(self, observer: Observer) -> None:
-        print("Subject: Attached an observer.")
+        print(f"{self._name}: Attached an observer.")
         self._observers.append(observer)
-
     def detach(self, observer: Observer) -> None:
+        print(f"{self._name}: Removed an observer.")
         self._observers.remove(observer)
-
-    """
-    The subscription management methods.
-    """
-
     def notify(self) -> None:
-        """
-        Trigger an update in each subscriber.
-        """
-
-        print("Subject: Notifying observers...")
+        print(f"{self._name}: Notifying observers...")
         for observer in self._observers:
             observer.update(self)
-
-    def some_business_logic(self) -> None:
-        """
-        Usually, the subscription logic is only a fraction of what a Subject can
-        really do. Subjects commonly hold some important business logic, that
-        triggers a notification method whenever something important is about to
-        happen (or after it).
-        """
-
-        print("\nSubject: I'm doing something important.")
-        self._state = randrange(0, 10)
-
-        print(f"Subject: My state has just changed to: {self._state}")
+    def announcement(self, state) -> None:
+        self._state = state
+        print(f"{self._name}: Announces {self._state}")
         self.notify()
 
-
 class Observer(ABC):
-    """
-    The Observer interface declares the update method, used by subjects.
-    """
-
     @abstractmethod
     def update(self, subject: Subject) -> None:
-        """
-        Receive update from subject.
-        """
         pass
 
-
-"""
-Concrete Observers react to the updates issued by the Subject they had been
-attached to.
-"""
-
-
 class City(Observer):
+    _name: str = ""
+    def __init__(self, name) -> None:
+        self._name=name
     def update(self, subject: Subject) -> None:
-        if subject._state < 3:
-            print("City: Reacted to the event")
+        print(f"{self._name} was updated")
 
 if __name__ == "__main__":
-    # The client code.
-
-    agency = Agency()
-
-    nyc = City()
-    agency.attach(nyc)
-
-    lax = City()
-    agency.attach(lax)
-
-    agency.some_business_logic()
-    agency.some_business_logic()
-
-    agency.detach(nyc)
-
-    agency.some_business_logic()
+    nws = Agency("Nation Weather Service")
+    nyc = City("New York City")
+    nws.attach(nyc)
+    lax = City("Los Angeles")
+    nws.attach(lax)
+    chi = City("Chicago")
+    nws.attach(chi)
+    nws.announcement("it's windy in the Midwest")
+    nws.detach(nyc)
+    nws.announcement("it's sunny in New England")
+    nws.announcement("it's snowy in the Southwest")
